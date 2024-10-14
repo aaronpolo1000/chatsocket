9c9f36f04d8b863b24dec97bd65e196fa40b25f8
A-03 <A-03@A-3>
2024-10-14 20:53:36 -0300

    sdaasd

--- src/main/java/chat/WebSocketConfig.java
+++ src/main/java/chat/WebSocketConfig.java
@@ -11,12 +11,12 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 public class WebSocketConfig {
 
 	
-	@Override
+	
 	public void configureMessageBroker(MessageBrokerRegistry registry) {
 		registry.enableSimpleBroker("/topic");
 		registry.setApplicationDestinationPrefixes("/app");
 	}
-	@Override
+
 	public void registerStompEndpoints(StompEndpointRegistry registry) {
 		registry.addEndpoint("/chats");
 	}
